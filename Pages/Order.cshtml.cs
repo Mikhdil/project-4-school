865d05545dbd3c7a16acd120ea9772802ce8fdfe
Киршин Максим <hankir@live.ru>
2022-01-07 17:01:05 +0400

    Заготовка сервера приложения.

---
+++ Pages/Order.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace TopSite.Pages
+{
+    public class OrderModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
